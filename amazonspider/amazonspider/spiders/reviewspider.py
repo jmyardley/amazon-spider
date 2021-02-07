ffa75ae95057b164d76ac34d1b1f3c9892690f30
James Y <35856061+jmyardley@users.noreply.github.com>
2021-02-07 13:43:03 -0500

    fix text extract & reviews path

--- amazonspider/amazonspider/spiders/reviewspider.py
+++ amazonspider/amazonspider/spiders/reviewspider.py
@@ -3,11 +3,11 @@ import scrapy
 
 class ReviewspiderSpider(scrapy.Spider):
     name = 'reviewspider'
-    allowed_domains = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7']
-    start_urls = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/']
+    allowed_domains = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
+    start_urls = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
 
     def start_requests(self):
-        url = "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/"
+        url = "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1"
 
         headers =  {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
@@ -24,7 +24,7 @@ class ReviewspiderSpider(scrapy.Spider):
                 'title': review.css('.review-title').css('span::text').get(),
                 'date': review.css('.review-date::text').extract(),
                 'rating': review.css('.review-rating').css('span::text').extract(),
-                'text': review.css('.review-text-content').css('span::text').extract()
+                'text': review.css('.review-text-content').css('span::text')[1].extract()
             }
             
             yield obj
