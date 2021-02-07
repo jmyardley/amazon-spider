7a714b70dfc59d33b310e6195924fae1a8ac3342
James Y <35856061+jmyardley@users.noreply.github.com>
2021-02-06 20:42:39 -0500

    configure headers

--- amazonspider/amazonspider/spiders/reviewspider.py
+++ amazonspider/amazonspider/spiders/reviewspider.py
@@ -4,7 +4,20 @@ import scrapy
 class ReviewspiderSpider(scrapy.Spider):
     name = 'reviewspider'
     allowed_domains = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7']
-    start_urls = ['http://https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/']
+    start_urls = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/']
+
+    def start_requests(self):
+        url = "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/"
+
+        headers =  {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
+            }
+        yield scrapy.http.Request(url, headers=headers)
+
 
     def parse(self, response):
-        pass
+        REVIEW = '. a-section review'
+        for review in response.css(REVIEW):
+            yield {
+            }
+
